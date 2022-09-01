1c49f5119d359da8306b1754ea806fff5c86e924
Ian Gaplichnik <igaplich@c3r5p6.hive.fi>
2022-09-01 17:25:34 +0300

    first own branch push

--- includes/lem_in.h
+++ includes/lem_in.h
@@ -15,11 +15,19 @@
 
 # include "../libftprintf/includes/ft_printf.h"
 
+typedef struct s_rooms
+{
+	char			*name;
+	struct s_rooms	*next;
+}	t_rooms;
+
 typedef struct s_data
 {
 	unsigned long	ants;
-	char			**rooms;
-	unsigned int	rooms_amount;
+	struct s_rooms	*head;
+	struct s_rooms	*tail;
+	struct s_rooms	*start;
+	struct s_rooms	*end;
 }					t_data;
 
 #endif
--- src/main.c
+++ src/main.c
@@ -12,17 +12,56 @@
 
 #include "../includes/lem_in.h"
 
+int	build_room_list(t_data *data, char *name)
+{
+	if (!data->head)
+	{
+		data->head = (t_rooms *)malloc(sizeof(t_rooms));
+		data->tail = data->head;
+		data->head->next = NULL;
+	}
+	else
+	{
+		data->tail->next = (t_rooms *)malloc(sizeof(t_rooms));
+		data->tail = data->tail->next;
+		data->tail->next = NULL;
+	}
+	data->tail = name;
+}
+
+int	read_room_name(t_data *data, char *line)
+{
+	int	i;
+
+	i = 0;
+	while (line[i] && line[i] != ' ')
+		i++;
+	build_room_list(data, ft_strsub(line, 0, i));
+}
+
+// int	read_comment(t_data *data, char *line)
+// {
+// 	if (ft_strncmp("##start", line, 7))
+// 		read_start(data, line);
+// 	else if (ft_strncmp("##end", line, 5))
+// 		read_end(data, line);
+// 	else
+// 		return (1);
+// }
+
 int	read_rooms(t_data *data)
 {
 	char	*line;
 
 	while (get_next_line(0, &line))
 	{
-		if (*line == '#')
-		{
-			if (read_comment(data, line))
-				return (-1);
-		}
+		// if (line && *line == '#')
+		// {
+		// 	if (read_comment(data, line))
+		// 		return (-1);
+		// }
+		// else
+			read_room_name(data, line);
 	}
 }
 
@@ -31,5 +70,10 @@ int main(void)
 	t_data	data;
 
 	read_rooms(&data);
+	// init_struct(&data);
+	// read_ants();
+	// read_rooms();
+	// read_links();
+
 	return (0);
 }
