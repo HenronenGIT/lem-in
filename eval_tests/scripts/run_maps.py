bbfcd29f6e6db808c469d8644e39c9240e85a15f
Henri <henrimaronen@gmail.com>
2022-10-07 13:49:12 +0300

    script updated

--- eval_tests/scripts/run_maps.py
+++ eval_tests/scripts/run_maps.py
@@ -13,9 +13,8 @@ files_arr = glob.glob("eval_tests/test_maps/*.map")
 
 i = 0
 for file in files_arr:
-	i += 1
-	print(file)
-	print("===== " + file +" =====")
-	input("Press Enter to continue...")
+	path_array = file.split("/")
+	print("===== " + path_array[2] +" =====", flush=True)
+	# input("Press Enter to continue...")
 	os.system(lem_in + "<" + file)
 	# os.system(lem_in + "<" + file + ">" + (f"correct_{i}.txt"))
