20f49b5a08b7c14c10d51fdf8028255d0517eb3d
Henri <henrimaronen@gmail.com>
2022-10-18 14:02:11 +0300

    set_flow_utils.c cleaned for norm

--- src/set_flow_utils.c
+++ src/set_flow_utils.c
@@ -12,9 +12,9 @@
 
 #include "../includes/lem_in.h"
 
-void allocate_flow_pointer(t_data *data, t_room *current)
+void	allocate_flow_pointer(t_data *data, t_room *current)
 {
-	size_t i;
+	size_t	i;
 
 	i = 0;
 	while (data->start->flow[i])
@@ -22,15 +22,25 @@ void allocate_flow_pointer(t_data *data, t_room *current)
 	data->start->flow[i] = current;
 }
 
-void allocate_flow_from_pointer(t_room *current, t_room *parent)
+void	allocate_flow_from_pointer(t_room *current, t_room *parent)
 {
 	current->flow_from = parent;
 }
 
-void set_flows(t_data *data)
+/*
+		set_flows() is basically backtracking function.
+	We start backstracking from end room and our goal is to find start room again.
+	We have 2 different cases when we are deciding which way we want to go:
+		1. Room has only parent pointer initialized.
+		2. Room have parent pointer AND flow_parent pointer initialized.
+	We always prioritize a room which is case 2.
+	Otherwise we move towards room which is case 1.
+		Same time when we backtrack we set flows and also cut "double flows".
+*/
+void	set_flows(t_data *data)
 {
-	t_room *current;
-	t_room *parent;
+	t_room	*current;
+	t_room	*parent;
 
 	current = data->end;
 	parent = current->parent;
@@ -38,16 +48,13 @@ void set_flows(t_data *data)
 	{
 		if (parent == data->start)
 			allocate_flow_pointer(data, current);
-		// if (current->flow_parent) //* old if
-		//* This if fixed map 5 issue
-		if (current->flow_parent && current->flow_parent->flow_from == current) //* move towards parent and cut the flow.
+		if (current->flow_parent && current->flow_parent->flow_from == current)
 		{
 			current->flow_parent->flow_from = NULL;
 			current = current->flow_parent;
 			parent = current->parent;
-			// current->flow_parent = NULL; //! breaks map augment_7, but  but fixes augment_5
 		}
-		else //* move towards parent
+		else
 		{
 			if (parent != data->start)
 				*parent->flow = current;
